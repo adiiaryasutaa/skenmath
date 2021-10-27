7c4afb0f0c0b78096f52e2eef3549540e5b880ed
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2021-10-27 14:57:20 +0800

    testing custome elements 1

--- dist/components/footer.js
+++ dist/components/footer.js
@@ -3,11 +3,19 @@ class Footer extends HTMLElement {
 
     super();
 
+    let
+      brandLogoSrc = this.hasAttribute('brand-logo-src') ? this.getAttribute('brand-logo-src') : './',
+      brandHref    = this.hasAttribute('brand-href') ? this.getAttribute('brand-href') : '#',
+      homePageHref = this.hasAttribute('home-page-href') ? this.getAttribute('home-page-href') : '#',
+      materialHref = this.hasAttribute('material-href') ? this.getAttribute('material-href') : '#',
+      aboutUsHref  = this.hasAttribute('about-us-href') ? this.getAttribute('about-us-href') : '#',
+      lisenceHref  = this.hasAttribute('lisence-href') ? this.getAttribute('lisence-href') : '#';
+
     this.innerHTML = `    
-      <footer id="footer" class="container-fluid bg-dark text-white py-5">
+      <footer id="footer" class="container-fluid bg-dark py-5">
         <div class="container d-flex flex-column flex-lg-row justify-content-between">
           <div class="mb-4 mb-lg-0 me-3">
-            <h3><img id="skenmath-logo" src="assets/img/skenmath-logo-design-1.svg" alt="skenmath-logo"> <span class="fw-light">SKEN</span><span class="fw-bold">MATH</span></h3>
+            <h3><img id="skenmath-logo" src="${brandLogoSrc}" alt="skenmath-logo"> <span class="fw-light">SKEN</span><span class="fw-bold">MATH</span></h3>
             <ul class="list-unstyled">
               <li class="mb-3"><span class="fw-light">SKEN</span><span class="fw-bold">MATH</span> is maintained by <span class="fw-light">SKEN</span><span class="fw-bold">MATH</span> team.</li>
               <li>© Copyright 2021 <span class="fw-light">SKEN</span><span class="fw-bold">MATH</span> team. All rights reserved.</li>
@@ -18,10 +26,10 @@ class Footer extends HTMLElement {
               <div class="">
                 <h5>NAVIGATIONS</h5>
                 <ul class="list-unstyled">
-                  <li><a class="text-white-50 text-decoration-none" href="">Halaman utama</a></li>
-                  <li><a class="text-white-50 text-decoration-none" href="materi/">Materi</a></li>
-                  <li><a class="text-white-50 text-decoration-none" href="">Tentang kami</a></li>
-                  <li><a class="text-white-50 text-decoration-none" href="https://github.com/adiiaryasutaa/skenmath/blob/main/LICENSE">Lisensi</a></li>
+                  <li><a class="text-white-50 text-decoration-none" href="${homePageHref}">Halaman utama</a></li>
+                  <li><a class="text-white-50 text-decoration-none" href="${materialHref}">Materi</a></li>
+                  <li><a class="text-white-50 text-decoration-none" href="${aboutUsHref}">Tentang kami</a></li>
+                  <li><a class="text-white-50 text-decoration-none" href="${lisenceHref}">Lisensi</a></li>
                 </ul>
               </div>
               <div class="mx-3">
@@ -47,3 +55,5 @@ class Footer extends HTMLElement {
     `;
   }
 }
+
+customElements.define('custome-footer', Footer);
