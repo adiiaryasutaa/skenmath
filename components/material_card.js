32e680ae50c9411363440f6c5f4ec038f52bba31
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2021-10-31 16:45:51 +0800

    filling content on material page

--- components/material_card.js
+++ components/material_card.js
@@ -4,14 +4,14 @@ class MaterialCard extends HTMLElement {
     super();
 
     let
-      cardHref = this.hasAttribute('card-href') ? `href="${this.getAttribute('card-href')}"` : '',
+      cardHref = this.hasAttribute('card-href') && !this.getAttribute('card-href').length < 1 ? `href="${this.getAttribute('card-href')}"` : '',
       cardImgSrc = this.hasAttribute('card-img-src') ? this.getAttribute('card-img-src') : '',
       cardImgAlt = this.hasAttribute('card-img-alt') ? this.getAttribute('card-img-alt') : 'no-img',
       cardTitle = this.hasAttribute('card-title') ? this.getAttribute('card-title') : 'No Title :)';
 
     this.innerHTML = `
       <a class="text-decoration-none" ${cardHref}>
-        <div class="material-card card bg-light h-100">
+        <div class="material-card card bg-light shadow-sm h-100">
           <img src="${cardImgSrc}" class="card-img-top" alt="${cardImgAlt}">
           <div class="card-body">
             <h5 class="card-title">${cardTitle}</h5>
